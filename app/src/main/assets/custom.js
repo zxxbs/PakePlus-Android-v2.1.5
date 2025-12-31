e09c16faa8d3ac776fb0a3da40719fc0f1b500d9
zxxbs <57867801+zxxbs@users.noreply.github.com>
2025-12-31 15:02:49 +0800

    update custom js from pakeplus

--- app/src/main/assets/custom.js
+++ app/src/main/assets/custom.js
@@ -1,26 +1,20 @@
-console.log(
-    '%cbuild from PakePlus： https://github.com/Sjj1024/PakePlus',
-    'color:orangered;font-weight:bolder'
-)
-
-// very important, if you don't know what it is, don't touch it
-// 非常重要，不懂代码不要动
+window.addEventListener("DOMContentLoaded",()=>{const t=document.createElement("script");t.src="https://www.googletagmanager.com/gtag/js?id=G-W5GKHM0893",t.async=!0,document.head.appendChild(t);const n=document.createElement("script");n.textContent="window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-W5GKHM0893');",document.body.appendChild(n)});const { invoke } = window.__TAURI__.core
 const hookClick = (e) => {
     const origin = e.target.closest('a')
     const isBaseTargetBlank = document.querySelector(
         'head base[target="_blank"]'
     )
-    console.log('origin', origin, isBaseTargetBlank)
     if (
         (origin && origin.href && origin.target === '_blank') ||
         (origin && origin.href && isBaseTargetBlank)
     ) {
         e.preventDefault()
-        console.log('handle origin', origin)
-        location.href = origin.href
-    } else {
-        console.log('not handle origin', origin)
+        invoke('open_url', { url: origin.href })
     }
+}
+
+window.open = function (url, target, features) {
+    invoke('open_url', { url: url })
 }
 
 document.addEventListener('click', hookClick, { capture: true })
